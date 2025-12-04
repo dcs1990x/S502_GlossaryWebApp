e600666b485259d9cecff83b48f6fa146e87f338
Daniel Caldito Serrano <dcs1990x@gmail.com>
2025-12-04 12:34:35 +0100

    Initial commit

---
+++ src/main/java/com/glossary_app/GlossaryAppApplication.java
@@ -0,0 +1,13 @@
+package com.glossary_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlossaryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlossaryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glossary_app/GlossaryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glossary_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlossaryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
