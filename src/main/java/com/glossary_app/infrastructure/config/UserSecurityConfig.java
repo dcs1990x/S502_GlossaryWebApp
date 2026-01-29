dfc32551db6bd072fe0925f71089c2882708536b
Daniel Caldito Serrano <dcs1990x@gmail.com>
2026-01-29 09:27:34 +0100

    feat: implemented remaining service methods.

--- src/main/java/com/glossary_app/infrastructure/config/UserSecurityConfig.java
+++ src/main/java/com/glossary_app/infrastructure/config/UserSecurityConfig.java
@@ -1,23 +1,39 @@
 package com.glossary_app.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 
 @EnableWebFluxSecurity
+@Configuration
 public class UserSecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(
-            ServerHttpSecurity http) {
-        http.csrf().disable()
-                .authorizeExchange()
-                .pathMatchers(HttpMethod.POST, "/users/update").hasRole("ADMIN")
-                .pathMatchers("/**").permitAll()
-                .and()
-                .httpBasic();
-        return http.build();
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new MapReactiveUserDetailsService(user);
     }
 }
